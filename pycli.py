04b02cfcec367e4186da6552b6f0138041b8cafb
MM02-sudo <malcolmdndj@gmail.com>
2026-01-18 16:08:31 +0100

    start of add section

--- pycli.py
+++ pycli.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+import argparse
 
-def my_func():
-    print("test")
+parser = argparse.ArgumentParser(description="Py cli tool")
+
+parser.add_argument('command',
+                    choices=['add', 'list 'search', 'delete'],
+                    help='Usable commands')
+
+args = parser.parse_args()
+
+print(f"You entered command: {args.command}")
 
 
-if __name__ == "__main__":
-    print("scrpt was run directly")
-    my_func()
